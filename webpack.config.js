28dfe358ba6b41232b9be97f799e84337adfca70
Danilo Martinelli <danilomartinelli429@gmail.com>
2018-02-12 11:57:05 -0300

    refactor(webpack.config.js):   add comments

--- webpack.config.js
+++ webpack.config.js
@@ -98,9 +98,15 @@ module.exports = {
         new webpack.DefinePlugin({
             "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development")
         }),
-        // new CopyWebpackPlugin([
-        //     { from: "src/file.type" }
-        // ]),
+        /* 
+            Copy files from src to dist
+        */
+            // new CopyWebpackPlugin([
+            //     { from: "src/file.type" }
+            // ]),
+        /* 
+            END
+        */
         new HtmlWebpackPlugin({
             template: "./src/index.html",
             title: "React Boilerplate",
