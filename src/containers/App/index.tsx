fe6e1ccc6d766428bc118e33454b26721c0f5d3f
Danilo Martinelli <danilomartinelli@outlook.com.br>
2018-05-04 09:33:05 -0300

    feat(App): change function to class

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -8,14 +8,19 @@ import { About } from "../About";
 
 require("./style.scss");
 
-const App = () =>
-	<React.Fragment>
-		<Header />
-		<Switch>
-			<Route exact path="/" render={() => <Redirect to="/home" />} />
-			<Route path="/home" component={Home} />
-			<Route path="/about" component={About} />
-		</Switch>
-	</React.Fragment>;
+class App extends React.Component {
+	public render() {
+		return (
+			<React.Fragment>
+				<Header />
+				<Switch>
+					<Route exact path="/" render={() => <Redirect to="/home" />} />
+					<Route path="/home" component={Home} />
+					<Route path="/about" component={About} />
+				</Switch>
+			</React.Fragment>
+		);
+	}
+}
 
 export default hot(module)(App);
