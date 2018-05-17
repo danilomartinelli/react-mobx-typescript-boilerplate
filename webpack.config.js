1a8eab08e2b5ed58b4bb724a080c75a09ca36a7a
Danilo Martinelli <danilomartinelli@outlook.com.br>
2018-05-17 10:11:07 -0300

    feat(webpack.config.js): Add env  variable to PORT

--- webpack.config.js
+++ webpack.config.js
@@ -11,123 +11,125 @@ const PORT = process.env.PORT || 3000;
 const FAVICON_DIR = "./src/assets/favicon/favicon.png";
 
 module.exports = {
-    entry: [
-        "./src/index.tsx"
-    ],
-    mode: DEV_MODE ? "development" : "production",
-    devtool: DEV_MODE ? "source-map" : "",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/, exclude: /node_modules/, use: [{
-                    loader: "ts-loader",
-                    options: {
-                        configFile: DEV_MODE ? "tsconfig.json" : "tsconfig.deploy.json"
-                    }
-                }]
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                loader: combineLoaders([
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        query: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: "[name]__[local]___[hash:base64:5]"
-                        }
-                    },
-                    {
-                        loader: "sass-loader",
-                        query: {
-                            includePaths: [
-                                "./src"
-                            ]
-                        }
-                    }
-                ])
-            },
-            {
-                test: /\.css$/,
-                loader: combineLoaders([
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        query: {
-                            modules: false,
-                            importLoaders: 1,
-                            localIdentName: "[name]__[local]___[hash:base64:5]"
-                        }
-                    }
-
-                ])
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {}
-                    }
-                ]
-            },
-            {
-                test: /\.(ttf|eot|woff)(\?v=[0-9].[0-9].[0-9])?$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {}
-                    }
-                ]
-            },
-        ]
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js", ".json"]
-    },
-    output: {
-        path: __dirname + "/dist",
-        publicPath: "/",
-        filename: "bundle.[hash].js"
-    },
-    devServer: {
-        contentBase: "./dist",
-        compress: true,
-        port: PORT,
-        historyApiFallback: true,
-        open: true,
-    },
-    plugins: [
-        ...(!DEV_MODE ? [new UglifyJsPlugin()] : []),
-        new webpack.DefinePlugin({
-            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development")
-        }),
-        /*
+	entry: ["./src/index.tsx"],
+	mode: DEV_MODE ? "development" : "production",
+	devtool: DEV_MODE ? "source-map" : "",
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "ts-loader",
+						options: {
+							configFile: DEV_MODE ? "tsconfig.json" : "tsconfig.deploy.json",
+						},
+					},
+				],
+			},
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				loader: combineLoaders([
+					{
+						loader: "style-loader",
+					},
+					{
+						loader: "css-loader",
+						query: {
+							modules: true,
+							importLoaders: 1,
+							localIdentName: "[name]__[local]___[hash:base64:5]",
+						},
+					},
+					{
+						loader: "sass-loader",
+						query: {
+							includePaths: ["./src"],
+						},
+					},
+				]),
+			},
+			{
+				test: /\.css$/,
+				loader: combineLoaders([
+					{
+						loader: "style-loader",
+					},
+					{
+						loader: "css-loader",
+						query: {
+							modules: false,
+							importLoaders: 1,
+							localIdentName: "[name]__[local]___[hash:base64:5]",
+						},
+					},
+				]),
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {},
+					},
+				],
+			},
+			{
+				test: /\.(ttf|eot|woff)(\?v=[0-9].[0-9].[0-9])?$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {},
+					},
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js", ".json"],
+	},
+	output: {
+		path: __dirname + "/dist",
+		publicPath: "/",
+		filename: "bundle.[hash].js",
+	},
+	devServer: {
+		contentBase: "./dist",
+		compress: true,
+		port: PORT,
+		historyApiFallback: true,
+		open: true,
+	},
+	plugins: [
+		...(!DEV_MODE ? [new UglifyJsPlugin()] : []),
+		new webpack.DefinePlugin({
+			"process.env.NODE_ENV": JSON.stringify(
+				process.env.NODE_ENV || "development",
+			),
+			"process.env.PORT": JSON.stringify(PORT),
+		}),
+		/*
             Copy files from src to dist
         */
-        // new CopyWebpackPlugin([
-        //     { from: "src/file.type" }
-        // ]),
-        /*
+		// new CopyWebpackPlugin([
+		//     { from: "src/file.type" }
+		// ]),
+		/*
             END
         */
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            filename: "index.html",
-        }),
-        new WebappWebpackPlugin(FAVICON_DIR),
-        new ImageminPlugin({
-            disable: DEV_MODE,
-            pngquant: {
-                quality: "95-100"
-            },
-            test: /\.(jpe?g|png|gif|svg)$/i
-        }),
-    ]
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+			filename: "index.html",
+		}),
+		new WebappWebpackPlugin(FAVICON_DIR),
+		new ImageminPlugin({
+			disable: DEV_MODE,
+			pngquant: {
+				quality: "95-100",
+			},
+			test: /\.(jpe?g|png|gif|svg)$/i,
+		}),
+	],
 };
