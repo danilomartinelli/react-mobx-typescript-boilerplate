a4a092ba33029db2b94c948c7873b9c1c5ef1297
Danilo Leone <danilomartinelli429@gmail.com>
2019-01-29 15:49:31 -0300

    feat(About): Added new feature

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -6,68 +6,69 @@ const reactImg = require("../../assets/img/react.svg");
 const mobxImg = require("../../assets/img/mobx.png");
 
 const Container = styled.div`
-	padding: 20px;
+  padding: 20px;
 `;
 const Features = styled.div`
-	display: flex;
-	justify-content: space-around;
+  display: flex;
+  justify-content: space-around;
 `;
 const Content = styled.div`
-	color: ${colors.gray};
+  color: ${colors.gray};
 `;
 const Title = styled.span`
-	font-size: 20px;
-	color: ${colors.blue};
+  font-size: 20px;
+  color: ${colors.blue};
 `;
 const ImgContent = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: space-around;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 `;
 const Image = styled.img`
-	max-width: 200px;
-	max-height: 200px;
-	height: auto;
-	width: auto;
+  max-width: 200px;
+  max-height: 200px;
+  height: auto;
+  width: auto;
 `;
 
 export class About extends React.Component<{}, {}> {
-	public render() {
-		return (
-			<Container>
-				<Features>
-					<Content>
-						<Title>Features</Title>
-						<ul>
-							<li>React 16</li>
-							<li>React Hot Loader</li>
-							<li>Mobx 4</li>
-							<li>React Router 4</li>
-							<ul>
-								<li>React Router DOM</li>
-							</ul>
-							<li>Sass</li>
-							<li>Hot Reload</li>
-							<li>Source Map</li>
-							<li>Typescript 2.8</li>
-							<ul>
-								<li>ESNEXT</li>
-								<li>Linter</li>
-							</ul>
-							<li>Webpack 4</li>
-							<ul>
-								<li>Uglify</li>
-								<li>Imagemin</li>
-								<li>Favicon Plugin</li>
-							</ul>
-						</ul>
-					</Content>
-					<ImgContent>
-						<Image src={reactImg} alt="Logo React" />
-						<Image src={mobxImg} alt="Logo Mobx" />
-					</ImgContent>
-				</Features>
-			</Container>
-		);
-	}
+  public render() {
+    return (
+      <Container>
+        <Features>
+          <Content>
+            <Title>Features</Title>
+            <ul>
+              <li>React 16</li>
+              <li>React Hot Loader</li>
+              <li>Mobx 4</li>
+              <li>Styled Components</li>
+              <li>React Router 4</li>
+              <ul>
+                <li>React Router DOM</li>
+              </ul>
+              <li>Sass</li>
+              <li>Hot Reload</li>
+              <li>Source Map</li>
+              <li>Typescript 2.8</li>
+              <ul>
+                <li>ESNEXT</li>
+                <li>Linter</li>
+              </ul>
+              <li>Webpack 4</li>
+              <ul>
+                <li>Uglify</li>
+                <li>Imagemin</li>
+                <li>Favicon Plugin</li>
+              </ul>
+            </ul>
+          </Content>
+          <ImgContent>
+            <Image src={reactImg} alt="Logo React" />
+            <Image src={mobxImg} alt="Logo Mobx" />
+          </ImgContent>
+        </Features>
+      </Container>
+    );
+  }
 }
