31c2f54d257126d319f8075dab7c7c1585deb305
Danilo Leone <danilomartinelli429@gmail.com>
2019-01-29 15:44:00 -0300

    chore(package): Remove fonts packages

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -7,24 +7,20 @@ import { Home } from "../Home";
 import { About } from "../About";
 
 import "normalize.css";
-import { TypographyStyle, GoogleFont } from "react-typography";
-import { typography } from "../../utils/typography";
 
 class App extends React.Component {
-	public render() {
-		return (
-			<React.Fragment>
-				<TypographyStyle typography={typography} />
-				<GoogleFont typography={typography} />
-				<Header />
-				<Switch>
-					<Route exact path="/" render={() => <Redirect to="/home" />} />
-					<Route path="/home" component={Home} />
-					<Route path="/about" component={About} />
-				</Switch>
-			</React.Fragment>
-		);
-	}
+  public render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/home" />} />
+          <Route path="/home" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
 }
 
 export default hot(module)(App);
